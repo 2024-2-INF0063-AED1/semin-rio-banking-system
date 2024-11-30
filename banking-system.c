1f621e709435d96e7d77103f06e502b549340b89
Matheus Teles <matheusti770@gmail.com>
2024-11-30 20:42:22 -0300

    Inserindo as validacoes dos dados informados pelo usuario

--- banking-system.c
+++ banking-system.c
@@ -3,6 +3,7 @@
 #include<stdbool.h>
 #include <string.h>
 #include <time.h>
+#include <ctype.h>
 
 // Adicionando arquivos externos(modularização);
 #include "funcoesVoid.h"
@@ -15,8 +16,8 @@ typedef struct{
     char genero; 
     char telefone[12];
     char cpf[12];
-    float rendaMensal;
-    char tipoConta[12];
+    float saldo;
+    char tipoConta;
     int numeroConta;
     
 }Cliente;
@@ -77,18 +78,114 @@ void exibirClientes(No* cabeca) {
         printf("Genero: %c\n", atual->cliente.genero);
         printf("Telefone: %s\n", atual->cliente.telefone);
         printf("CPF: %s\n", atual->cliente.cpf);
-        printf("Renda Mensal: R$ %.2f\n", atual->cliente.rendaMensal);
-        printf("Tipo de Conta: %s\n", atual->cliente.tipoConta);
+        printf("Saldo da conta: R$ %.2f\n", atual->cliente.saldo);
+        printf("Tipo de Conta: %c\n", atual->cliente.tipoConta);
         printf("Numero da Conta: %d\n", atual->cliente.numeroConta);
         atual = atual->proximo;
     }
     barraNaTela();
 }
 
+// funcao com a logica de validacao do nome
+bool validaNome(Cliente *cliente){
+    if(strlen(cliente->nome) < 3){
+        printf("O nome deve ter pelo menos 3 caracteres");
+        return false;
+    }
+    for(int i = 0; cliente->nome[i] != '\0'; i++){
+        if((!isalpha(cliente->nome[i]) && !isspace(cliente->nome[i]))){
+
+             printf("O nome deve conter apenas letras e espacos\n");
+             return false;
+        }
+    }
+        
+    return true;
+    
+}
+
+// funcao com a logica de validacao da senha
+bool validaSenha(Cliente *cliente){
+    int temNumero = 0, temCaractere = 0, temMaiuscula = 0;
+    if(strlen(cliente->senha) < 8){
+        printf("A senha deve ter pelo menos 8 caracteres");
+        return false;
+    }
+    for(int i = 0; cliente->senha[i] != '\0'; i++){
+        if(isdigit(cliente->senha[i])){
+            temNumero = 1;
+        }
+        if(!isalnum(cliente->senha[i])){
+            temCaractere = 1;
+        }
+        if(isupper(cliente->senha[i])){
+            temMaiuscula = 1;
+        }
+    }
+    if((!temNumero) || (!temMaiuscula) || (!temCaractere)){
+        printf("A senha deve ter pelo menos um numero, uma maiuscula e um caractere\n");
+        return false;
+    }
+    
+    return true;
+}
+
+// funcao com a logica de validacao do genero
+
+bool validaGenero(Cliente *cliente){
+    cliente->genero = tolower(cliente->genero);
+    if((cliente->genero != 'm') && (cliente->genero != 'f') &&(cliente->genero != 'o')){
+        printf("Informe uma das opcoes validas");
+        return false;
+        
+    }
+    return true;
+}
+
+// funcao com a logica de validacao do telefone
+bool validaTelefone(Cliente *cliente){
+    if(strlen(cliente->telefone) != 11){
+        printf("O telefone deve ter 11 digitos");
+        return false;
+    }
+
+    for(int i = 0; cliente->telefone[i] != '\0'; i++){
+        if(!isdigit(cliente->telefone[i])){
+            printf("O telefone deve conter apenas numeros");
+            return false;
+        }
+    }
+    return true;
+
+}
+
+// funcao com a logica de validacao de tipo de conta
+bool validaTipoDeConta(Cliente *cliente){
+    cliente->tipoConta = tolower(cliente->tipoConta);
+    if((cliente->tipoConta != 'c') && (cliente->tipoConta != 'j') &&(cliente->tipoConta != 'p')){
+        printf("Informe uma das opcoes validas");
+        return false;
+        
+    }
+    return true;
+
+}
+
+//funcao com a logica de validacao do saldo informado pelo usuario
+bool validaSaldoDaConta(Cliente *cliente){
+    if(cliente->saldo < 0){
+        printf("Informe um valor valido para deposito inicial");
+        return false;
+    }
+    return true;
+}
+
+
+
 //Funçao com a logica para validacao do CPF informado
 bool validaCpf(Cliente *cliente){
     if(strlen(cliente->cpf) != 11){
-        printf("CPF invalido\n");
+        printf("O CPF deve ter 11 digitos e ser valido. Verifique e tente novamente.\n");
         return false;
     }
 
@@ -97,7 +194,7 @@ bool validaCpf(Cliente *cliente){
             (strcmp(cliente->cpf,"66666666666") == 0) || (strcmp(cliente->cpf,"77777777777") == 0) || (strcmp(cliente->cpf,"88888888888") == 0) ||
             (strcmp(cliente->cpf,"99999999999") == 0)){
         
-                printf("CPF invalido\n");
+                printf("O CPF não pode conter uma sequência de números repetidos. Verifique e tente novamente.\n");
                 return false;
     }
     else{
@@ -140,11 +237,10 @@ bool validaCpf(Cliente *cliente){
             dig2 = 11 - resto;
 
         if((cliente->cpf[9] - '0' == dig1) && (cliente->cpf[10] - '0' == dig2)){
-            printf("CPF VALIDADO!\n");
             return true;
         }
         else{
-            printf("CPF INVALIDO\n");
+            printf("CPF invalido. Por favor, insira um numero de CPF correto\n");
             return false;
         }
 
@@ -165,50 +261,65 @@ void criarConta (Cliente *cliente){
     barraNaTela();
     printf("\n           Criar  Conta       \n");
     barraNaTela();
-
-    printf("Nome: ");
-    getchar();
-    fgets(cliente->nome,sizeof(cliente->nome), stdin);
-    cliente->nome[strcspn(cliente->nome, "\n")] = '\0'; // remove o '\n' residual
-    puts(cliente->nome);
+    do{
+        printf("Nome: ");
+        fflush(stdin);
+        fgets(cliente->nome,sizeof(cliente->nome), stdin);
+        cliente->nome[strcspn(cliente->nome, "\n")] = '\0';
+        puts(cliente->nome);
+    }while(!validaNome(cliente));
+    
 
 
     printf("Email: ");
     scanf("%s", cliente->email);
     printf("%s\n",cliente -> email);
 
-    printf("Senha(max. 8): ");
-    scanf("%s", cliente->senha);
-    printf("%s\n", cliente->senha);
+    do{
+        printf("Senha(min. 8): ");
+        scanf("%s", cliente->senha);
+        printf("%s\n", cliente->senha);
+    }while(!validaSenha(cliente));
 
+    
 
-    printf("Genero feminino(F)/Masculino(M)/Outro(O): ");
-    getchar();
-    scanf("%c", &cliente->genero);
-    printf("%c\n",cliente->genero);
-
-    printf("Telefone:");
-    scanf("%s", cliente->telefone);
-    printf("%s\n",cliente->telefone);
+    do{
+        printf("Genero feminino(F)/Masculino(M)/Outro(O): ");
+        getchar();
+        scanf("%c", &cliente->genero);
+        printf("%c\n",cliente->genero);
+    }while(!validaGenero(cliente));
+    
+    do{
+        printf("Telefone:");
+        scanf("%s", cliente->telefone);
+        printf("%s\n",cliente->telefone);
+    }while(!validaTelefone(cliente));
+   
 
     do{
         printf("CPF: ");
         scanf("%s", cliente->cpf);
+        printf("%s\n", cliente->cpf);
     }while(!validaCpf(cliente));
     
+    do{
+        printf("Insira um valor para abrir a conta: R$ ");
+        scanf("%f", &cliente->saldo);
+        printf("%f\n", cliente->saldo);
+    }while(!validaSaldoDaConta(cliente));
     
+
+    do{
+        printf("Tipo De Conta:\n");
+        printf("[C] - Corrente;\n");
+        printf("[J] - Juridica;\n");
+        printf("[P] - Poupanca;\n");
+        getchar();
+        scanf("%c", &cliente->tipoConta);
+        printf("%c\n",cliente->tipoConta);
+    }while(!validaTipoDeConta(cliente));
     
-    printf("%s\n", cliente->cpf);
-
-    printf("Renda Mensal: R$ ");
-    scanf("%f", &cliente->rendaMensal);
-    printf("%f\n", cliente->rendaMensal);
-
-    printf("Tipo De Conta:\n");
-    printf("1. Corrente;\n");
-    printf("2. Juridica;\n");
-    printf("3. Poupanca;\n");
-    scanf("%s", cliente->tipoConta);
 
 
     numeroConta(cliente);
