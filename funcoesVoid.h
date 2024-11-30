68e90e7e40cf42c12c2423de91d3389468089bd4
VictorAlvesk <Victorfreire78965@gmail.com>
2024-11-30 11:37:05 -0300

    Resolvendo conflitos

---
+++ funcoesVoid.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include <string.h>
+#include <time.h>
+
+//Funçao para acessar a conta do usuario
+void acessarConta(){
+    printf("--------------------------------");
+    printf("\n          Acessar Conta       \n");
+    printf("--------------------------------\n");
+}
+
+void limparTela(){
+    system("CLS");
+}
+
+void barraNaTela(){
+    printf("\n--------------------------------\n");
+}
